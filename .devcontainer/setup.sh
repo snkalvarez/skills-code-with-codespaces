2c5d67d16a0501b08aa24ea91aa10d7856443bfa
jalvarez <61236470+snkalvarez@users.noreply.github.com>
2024-06-30 21:59:12 +0000

    hola

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Actualizando paquetes..." >> codespace.md
+
+echo "Estableciendo la zona horaria a Colombia/Bogotá..."
+sudo ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+sudo dpkg-reconfigure -f noninteractive tzdata
+
 sudo apt-get update
 sudo apt-get install sl
 alias sl="/usr/games/sl"
--- codespace.md
+++ codespace.md
@@ -2,3 +2,4 @@
 # Writing code upon codespace creation!
 # Writing code upon codespace creation!
 #ecribiendo en la consola
+Actualizando paquetes...
