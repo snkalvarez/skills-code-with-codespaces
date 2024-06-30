e10907eee3ee9a93d8fda5c035503726f1a607d7
jalvarez <61236470+snkalvarez@users.noreply.github.com>
2024-06-30 17:10:36 -0500

    hola

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,12 @@
 
 echo "Actualizando paquetes..." >> codespace.md
 
-echo "Estableciendo la zona horaria a Colombia/Bogotá..."
 sudo ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
 sudo dpkg-reconfigure -f noninteractive tzdata
+echo "Estableciendo la zona horaria a Colombia/Bogotá..." >> codespace.md
 
 sudo apt-get update
 sudo apt-get install sl
 alias sl="/usr/games/sl"
+
+echo "terminando la configuración..." >> codespace.md
